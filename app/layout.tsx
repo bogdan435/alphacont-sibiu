c61191701250aed75f6e98e25f8d42440c69e7a3
Bogdan <bogdan@alphacont.ro>
2026-04-20 20:57:49 +0300

    Reorder scroll to top in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,8 +49,8 @@ export default async function RootLayout({
           `}
         </Script>
         {children}
-        <AnalyticsWithConsent />
         <ScrollToTop />
+        <AnalyticsWithConsent />
         <CookieBanner locale={locale} />
       </body>
     </html>
