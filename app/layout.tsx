ce83c42ff839840a0ae9ef192eac93a94c947857
Bogdan <bogdan@alphacont.ro>
2026-04-20 19:29:00 +0300

    Set live Google Analytics ID

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default async function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
-      <GoogleAnalytics gaId="G-XXXXXXXXXX" />
+      <GoogleAnalytics gaId="G-JYCVFNBZ6S" />
     </html>
   );
 }
