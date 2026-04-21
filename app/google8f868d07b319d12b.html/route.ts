16957d895530ba286255a2abf62fb774b8f999a1
Bogdan <bogdan@alphacont.ro>
2026-04-21 13:34:46 +0300

    Update Google verification route response

--- app/google8f868d07b319d12b.html/route.ts
+++ app/google8f868d07b319d12b.html/route.ts
@@ -1,7 +1,7 @@
-export function GET() {
+export async function GET() {
   return new Response("google-site-verification: google8f868d07b319d12b.html", {
     headers: {
-      "Content-Type": "text/html; charset=utf-8",
+      "Content-Type": "text/html",
     },
   });
 }
